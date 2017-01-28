9a250016cff054156f23654387c3937478c34f9e
UdayOnGit <uday.k.jadhav@gmail.com>
2017-01-29 01:34:44 +1100

    Implemented Multiply function

--- HelloGit/NewClass.cs
+++ HelloGit/NewClass.cs
@@ -17,5 +17,10 @@ namespace HelloGit
         {
             return num1 - num2;
         }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
     }
 }
