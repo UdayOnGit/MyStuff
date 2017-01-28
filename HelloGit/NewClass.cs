1e40745c7e18a7447359ee8012e43badd72a00b8
UdayOnGit <uday.k.jadhav@gmail.com>
2017-01-29 01:34:05 +1100

    Implemented Subtract function

--- HelloGit/NewClass.cs
+++ HelloGit/NewClass.cs
@@ -12,5 +12,10 @@ namespace HelloGit
         {
             return num1 + num2;
         }
+
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
     }
 }
